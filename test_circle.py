806d06207533383962b020139ef5bf718667a667
Natcha Ranchan <natcha.ran@ku.th>
2024-03-12 18:18:24 +0700

    first commit

--- test_circle.py
+++ test_circle.py
@@ -7,6 +7,30 @@ Write unit tests as described in README.md.
 3. Unit test that circle constructor raises exception of radius is negative.
 
 """
-from circle import Circle
 
 # TODO write 3 tests as described above
+from circle import Circle
+import unittest
+import math
+class CircledTest(unittest.TestCase):
+    # def setUp(self):
+    #     """Create an auction before each test."""
+    #     self.circle = Circle()
+
+    def test_add_area(self):
+        c1 = Circle(2)
+        c2 = Circle(4)
+        new_circle = c1.add_area(c2)
+        total_area = c1.get_area()+c2.get_area()
+        # print(c1.get_area() + c2.get_area())
+        self.assertAlmostEqual(total_area,new_circle.get_area()) # check area
+        self.assertAlmostEqual(math.sqrt((c1.get_radius())**2 + (c2.get_radius())**2),new_circle.get_radius())
+        # print(new_circle.get_area())
+
+    def test_edge_area(self):
+        c1 = Circle(0)
+        c2 = Circle(4)
+        new_circle = c1.add_area(c2)
+        total_area = c1.get_area()+c2.get_area()
+        self.assertAlmostEqual(total_area,new_circle.get_area()) # check area
+        self.assertAlmostEqual(math.sqrt((c1.get_radius())**2 + (c2.get_radius())**2),new_circle.get_radius())
