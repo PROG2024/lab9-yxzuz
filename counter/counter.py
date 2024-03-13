c2ef9b4e41e8d1056393579bee041f667c9bf9fa
Natcha Ranchan <natcha.ran@ku.th>
2024-03-13 12:24:57 +0700

    counter with doctest

--- counter/counter.py
+++ counter/counter.py
@@ -10,13 +10,54 @@
 """
 
 class Counter:
+    """doctest
+    Exmaple
+    >>> counter = Counter()
+    >>> counter.count # invoking count doesn't change anything
+    1
+    >>> counter.count
+    1
+    >>> counter.increment() # add 1 and return the new count
+    2
+    >>> counter2 = Counter()
+    >>> counter2 is counter
+    True
+    >>> counter2.count # shares same count
+    2
+    >>> counter2.increment() # add 1 and return the new count
+    3
+    >>> counter.count
+    3
 
-    def __init__(self):
-        self.__count = 0
+    """
+    __instance = None
+    count_num = 1
+
+    def __new__(cls, *args, **kwargs):
+       """Allocate memory and return a reference to a new Counter obj"""
+       # Has a Counter been created before
+       if cls.__instance is None: # haven't created yet
+           # create the one and only counter
+           cls.__instance = super().__new__(cls)
+           cls.__instance.__count = cls.count_num
+       return cls.__instance
+
+    # def __init__(self):
+    #     print('Init')
+    #     self.__count = 1
+
+    def __str__(cls):
+        return f"{cls.count_num}"
+
+    @property
+    def count(self):
+        return self.__count
 
 
-    def __str__(self):
-        return f"{self.__count}"
+    def increment(self):
+        self.__count += 1
+        return self.count
 
-    #TODO write count property
-    #TODO write increment method
+if __name__ == '__main__':
+    c = Counter()
+    c
